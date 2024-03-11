5e7abb099f1bb5fd214b7664521d8e4c01a6d9c2
Kiril Katsarski <k.katsarski@student.fontys.nl>
2024-03-11 21:39:49 +0100

    small things

--- frontend/src/pages/homepage/HomePage.tsx
+++ frontend/src/pages/homepage/HomePage.tsx
@@ -7,7 +7,7 @@ import HomepageFeature from "../../components/homePageFeature/HomePageFeature";
 function HomePage() {
     return (
         <div className="container-lg bg-light pb-5">
-            <div className="row p-5 text-center mx-3 hero border-primary border">
+            <div className="row p-5 text-center mx-3 hero border-primary border" id="hero">
                 <div className="col-lg-6 mx-auto my-5 py-5">
                     <div className="text-shadow">
                         <div className="display-6 text-light">Welcome to</div>
@@ -18,17 +18,17 @@ function HomePage() {
             </div>
             <div className="row row-cols-1 row-cols-lg-3 p-4 pb-5 ps-5">
                 <HomepageFeature
-                    title={`1784 problem so far`}
+                    title={`1784 problems so far`}
                     description={"Embark on your journey to become a Python master by solving real-world problems and challenges."}
-                    icon={"bi-hand-index-thumb-fill"} />
+                    icon={"bi-file-earmark-code"} />
                 <HomepageFeature
                     title={"Beat the interviewers at their own game"}
                     description={"Our platform is designed to help you prepare for technical interviews and hackathons."}
-                    icon={"bi-check-circle-fill"} />
+                    icon={"bi-trophy"} />
                 <HomepageFeature
                     title={"Completely free for you"}
                     description={"We don't charge you anything for using our platform. You can use it for free, forever."}
-                    icon={"bi-piggy-bank-fill"} />
+                    icon={"bi-piggy-bank"} />
             </div>
             <div className="row bg-ccdarkblue py-4 mt-5 justify-content-center mx-5 rounded-3">
                 <div className="col-lg-3 justify-content-center">

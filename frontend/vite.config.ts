9e0968e043a585906b94673618dbf26632f8f174
Kiril Katsarski <k.katsarski@student.fontys.nl>
2024-03-21 12:00:12 +0100

    fix vite env

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,10 +1,25 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv} from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 3333
-  }
-})
+export default defineConfig(({ mode }) => {
+    // Load env file based on `mode` in the current working directory.
+    // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
+    const env = loadEnv(mode, process.cwd(), '');
+    const config = {
+      plugins: [react()],
+      server: {
+        open: true,
+        port: parseInt(env.VITE_PORT, 10) || 3000,
+      },
+    //   preview: {
+    //     headers:{
+    //       'Content-Security-Policy': 'upgrade-insecure-requests',
+    //       "X-Frame-Options": "DENY",
+    //       "X-XSS-Protection": "1; mode=block"
+    //     },
+    //     port: parseInt(env.PORT, 10) || 3000,
+    //   },
+    };
+  
+    return config;
+  });

39d8add48d5f4078d11e855f1e9357687b5c58be
Kiril Katsarski <k.katsarski@student.fontys.nl>
2024-04-03 17:06:02 +0200

    add script to create db if db doesnt exist

---
+++ problem-microservice/database/create-database/create-database.ts
@@ -0,0 +1,21 @@
+import { createConnection } from "typeorm";
+import { dataSourceOptions } from "../data-source";
+
+export async function createDatabaseIfNotExist() {
+    console.log('Creating database...')
+
+    const connectionOptions = {...dataSourceOptions, database: 'mysql'};
+    const connection = await createConnection(connectionOptions as any);
+    const queryRunner = connection.createQueryRunner();
+
+    try {
+        await queryRunner.connect();
+        await queryRunner.createDatabase(dataSourceOptions.database as string ?? 'problem-ms', true);
+        await queryRunner.release();
+        await connection.close();
+        console.log('Finished database creation script')
+    } catch(e) {
+        console.log('Could not create database');
+        console.error(e);
+    }
+}

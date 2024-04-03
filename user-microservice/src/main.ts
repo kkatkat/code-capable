0ed0da45492b1a3222642093af492e1fb0c4414e
Kiril Katsarski <k.katsarski@student.fontys.nl>
2024-04-03 17:23:02 +0200

    add node_env to env.examples

--- user-microservice/src/main.ts
+++ user-microservice/src/main.ts
@@ -4,6 +4,7 @@ import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
+    
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   const config = app.get(ConfigService);

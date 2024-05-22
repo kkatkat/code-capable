e9ba1c6bcaf20a91753f72cfb5f308bab4e987f2
Kiril Katsarski <k.katsarski@student.fontys.nl>
2024-05-22 20:08:17 +0200

    remove unused code

--- problem-microservice/src/modules/unit-test/unit-test.controller.ts
+++ problem-microservice/src/modules/unit-test/unit-test.controller.ts
@@ -1,51 +1,51 @@
-import { Body, Controller, Delete, Get, NotFoundException, Param, Post, Put } from "@nestjs/common";
-import { UnitTestService } from "./unit-test.service";
-import { UnitTest } from "./unit-test.entity";
-import { UnitTestInputDTO } from "./unit-testIput.dto";
-import { UnitTestUpdateDTO } from "./unit-testUpdate.dto";
+// import { Body, Controller, Delete, Get, NotFoundException, Param, Post, Put } from "@nestjs/common";
+// import { UnitTestService } from "./unit-test.service";
+// import { UnitTest } from "./unit-test.entity";
+// import { UnitTestInputDTO } from "./unit-testIput.dto";
+// import { UnitTestUpdateDTO } from "./unit-testUpdate.dto";
 
-@Controller('unit-test')
-export class UnitTestController {
-    constructor(private readonly unitTestService: UnitTestService) {}
+// @Controller('unit-test')
+// export class UnitTestController {
+//     constructor(private readonly unitTestService: UnitTestService) {}
 
-    @Get('/problem/:problemId')
-    async getForProblem(@Param('problemId') problemId: number): Promise<UnitTest[]> {
-        return this.unitTestService.find({
-            where: {
-                problem: {
-                    id: problemId
-                }
-            }
-        })
-    }
+//     @Get('/problem/:problemId')
+//     async getForProblem(@Param('problemId') problemId: number): Promise<UnitTest[]> {
+//         return this.unitTestService.find({
+//             where: {
+//                 problem: {
+//                     id: problemId
+//                 }
+//             }
+//         })
+//     }
 
-    @Get('/:id')
-    async getById(@Param('id') id: number) {
-        const unitTest = await this.unitTestService.findOne({
-            where: {
-                id
-            }
-        })
+//     @Get('/:id')
+//     async getById(@Param('id') id: number) {
+//         const unitTest = await this.unitTestService.findOne({
+//             where: {
+//                 id
+//             }
+//         })
 
-        if (!unitTest) {
-            throw new NotFoundException();
-        }
+//         if (!unitTest) {
+//             throw new NotFoundException();
+//         }
 
-        return unitTest;
-    }
+//         return unitTest;
+//     }
 
-    @Post()
-    async create(@Body() input: UnitTestInputDTO): Promise<UnitTest> {
-        return this.unitTestService.create(input);
-    }
+//     @Post()
+//     async create(@Body() input: UnitTestInputDTO): Promise<UnitTest> {
+//         return this.unitTestService.create(input);
+//     }
 
-    @Put()
-    async update(@Body() input: UnitTestUpdateDTO): Promise<UnitTest> {
-        return this.unitTestService.update(input);
-    }
+//     @Put()
+//     async update(@Body() input: UnitTestUpdateDTO): Promise<UnitTest> {
+//         return this.unitTestService.update(input);
+//     }
 
-    @Delete(':id')
-    async delete(@Param('id') id: number) {
-        return this.unitTestService.delete({id});
-    }
-}
+//     @Delete(':id')
+//     async delete(@Param('id') id: number) {
+//         return this.unitTestService.delete({id});
+//     }
+// }
--- problem-microservice/src/modules/unit-test/unit-test.module.ts
+++ problem-microservice/src/modules/unit-test/unit-test.module.ts
@@ -1,7 +1,7 @@
 import { Module, forwardRef } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UnitTest } from './unit-test.entity';
-import { UnitTestController } from './unit-test.controller';
+// import { UnitTestController } from './unit-test.controller';
 import { UnitTestService } from './unit-test.service';
 import { FactoryModule } from '../factory/factory.module';
 import { ConfigModule } from '@nestjs/config';
@@ -9,7 +9,7 @@ import { ConfigModule } from '@nestjs/config';
 @Module({
     imports: [TypeOrmModule.forFeature([UnitTest]), forwardRef(() => FactoryModule), ConfigModule],
     exports: [TypeOrmModule, UnitTestService],
-    controllers: [UnitTestController],
+    controllers: [],
     providers: [UnitTestService],
 })
 export class UnitTestModule {}
